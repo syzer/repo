c9a7be00242aeb35052ee0f553a9e0ff4fb0d014
syzer3@gmail.com <syzer3@gmail.com>
2016-09-27 13:22:55 +0200

    open repo

---
+++ repo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+// TODO real remote
+func main() {
+	_, err := exec.Command("ping", "-c 1", "gitlab.liip.ch").Output()
+	if err != nil {
+		log.Fatal("Network is down... or you forgot VPN")
+	}
+	remote := getRemote()
+
+	exec.Command("open", remote).Output()
+
+	log.Println("(╯°□°）╯︵ ┻━┻ ", remote)
+}
+
+func getRemote() string {
+	remote, err := exec.Command("git", "remote", "-v").Output()
+	if err != nil {
+		log.Fatal("Go inside git repo!")
+	}
+	url := string(remote)
+	r, _ := regexp.Compile("http([a-z]://.+)*.git")
+	url = strings.Replace(r.FindString(url), ".git", "", 1)
+	return url
+}
